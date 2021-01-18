9126a7b7ac554e6bbe92114c35acdfacd3f7c81a
frejonb <f.g.rejonbarrera@gmail.com>
2021-01-18 21:43:13 +0000

    expose docker tcp daemon

--- docker-scripts/docker-entrypoint.sh
+++ docker-scripts/docker-entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 cp -npr /root-container/. /root 
 service ssh restart
-service docker restart
+service docker stop
+echo '{"hosts": ["unix:///var/run/docker.sock", "tcp://127.0.0.1:2375"]}' > /etc/docker/daemon.json
+service docker start
 exec "$@"
