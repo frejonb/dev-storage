76c1e420a073c062ca743b28d63897b1a9ecaf47
frejonb <f.g.rejonbarrera@gmail.com>
2021-03-02 00:19:32 +0000

    update eksctl

--- docker-scripts/install-extra-packages.sh
+++ docker-scripts/install-extra-packages.sh
@@ -52,7 +52,7 @@ curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack
 mv kustomize /usr/local/bin
 
 # Eksctl
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/0.18.0/eksctl_Linux_amd64.tar.gz" | tar xz -C /tmp
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/0.39.0/eksctl_Linux_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
 
 # Terraform
